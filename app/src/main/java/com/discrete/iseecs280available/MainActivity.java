e0aa2569b8f0d1fa3c647fc234c5246db882aad2
Aakash Patel <aakashdp@umich.edu>
2017-12-23 00:15:16 -0500

    dropdown selection works for all schools and ENG subjects

--- app/src/main/java/com/discrete/iseecs280available/MainActivity.java
+++ app/src/main/java/com/discrete/iseecs280available/MainActivity.java
@@ -1,5 +1,6 @@
 package com.discrete.iseecs280available;
 
+import android.os.Environment;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -7,29 +8,39 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 import org.json.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    String sectionType = "LEC";
-    String termCode = "2160";
-    String schoolCode = "ENG";
-    String subjectCode = "";
-    String catalogNum = "";
+    private String termCode = "2170";
+    private String schoolCode = "";
+    private String subjectCode = "";
+    private String catalogNum = "";
 
-    Button submitButton;
-    EditText editDepartment, editCourse;
+    public Button submitButton;
+    public AutoCompleteTextView editSubject, editSchool;
+
+    private static Map<String, String> schoolData, subjectData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,45 +49,101 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
 
             submitButton = (Button)findViewById(R.id.submitCourseInfo);
-            editDepartment   = (EditText)findViewById(R.id.enterDepartment);
-            editCourse = (EditText)findViewById(R.id.enterCourseNumber);
+            editSchool = (AutoCompleteTextView)findViewById(R.id.enterSchool);
+            editSubject = (AutoCompleteTextView)findViewById(R.id.enterSubject);
 
-            submitButton.setOnClickListener(
-                    new View.OnClickListener()
-                    {
-                        public void onClick(View view)
-                        {
-                            subjectCode = editDepartment.getText().toString();
-                            catalogNum = editCourse.getText().toString();
 
-                            System.out.println(subjectCode);
-                            System.out.println(catalogNum);
+            // create map of schools and their codes
+            schoolData = readMapFromFile(
+                    "schools.json",
+                    "SchoolDescr",
+                    "SchoolCode");
+            final Set<String> SCHOOLS = schoolData.keySet();
 
-                            if (!subjectCode.isEmpty() && !catalogNum.isEmpty()) {
-                                TextView header = (TextView) findViewById(R.id.courseDisplayHeader);
-                                TextView info = (TextView) findViewById(R.id.courseInfo);
-                                try {
-                                    String openSections = findOpenCourses();
+            // create array adapter for school input field with school names
+            final ArrayAdapter<String> school_adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_dropdown_item_1line,
+                    SCHOOLS.toArray(new String[SCHOOLS.size()]));
+            editSchool.setAdapter(school_adapter);
+            editSchool.setThreshold(1);
 
-                                    String headerMessage = subjectCode + " " + catalogNum;
-                                    header.setText(headerMessage);
+            // create array adapter for subject field, currently empty
+            final ArrayAdapter<String> subject_adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_dropdown_item_1line, new String[0]);
+            editSubject.setAdapter(subject_adapter);
+            editSubject.setThreshold(1);
 
-                                    info.setText(openSections);
-                                    info.setMovementMethod(new ScrollingMovementMethod());
+
+            submitButton.setOnClickListener( new View.OnClickListener() {
+                public void onClick(View view) {
+
+                    // get school and subject values
+                    schoolCode = schoolData.get(editSchool.getText().toString());
+                    subjectCode = editSubject.getText().toString();
+
+/*                    if (!subjectCode.isEmpty() && !catalogNum.isEmpty()) {
+                        TextView header = (TextView) findViewById(R.id.courseDisplayHeader);
+                        TextView info = (TextView) findViewById(R.id.courseInfo);
+                        try {
+                            String openSections = findOpenCourses();
+
+                            String headerMessage = subjectCode + " " + catalogNum;
+                            header.setText(headerMessage);
+
+                            info.setText(openSections);
+                            info.setMovementMethod(new ScrollingMovementMethod());
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                            // clear header
+                            String headerMessage = "";
+                            header.setText(headerMessage);
+
+                            String errorMessage = "Sorry, the course you specified could not be found.";
+                            info.setText(errorMessage);
+                        }
+                    }*/
+
+                    }
+                }
+            );
+
+            // EFFECTS: when the focus moves from school field to subject field, populate
+            //          the subject adapter with the school department names
+            editSchool.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                public void onFocusChange(View view, boolean hasFocus) {
+                    if (!hasFocus) {
+                        // check if the school field is filled
+                        String school = editSchool.getText().toString();
+                        if (!school.equals(null)) {
+                            schoolCode = schoolData.get(school);
+                            editSubject.setText("");
+                        }
+                        // check if the school entered is valid
+                        if (schoolCode != null) {
+                            try {
+                                subject_adapter.clear();
+                                subjectData = readMapFromFile(
+                                        schoolCode + ".json",
+                                        "SubjectDescr",
+                                        "SubjectCode"
+                                );
+                                // add values to subject adapter in form
+                                // [School Name (Code)]
+                                for (Map.Entry<String, String> entry : subjectData.entrySet()) {
+                                    subject_adapter.add(entry.getValue() + " - " + entry.getKey());
                                 }
-                                catch (Exception e) {
-                                    e.printStackTrace();
-                                    // clear header
-                                    String headerMessage = "";
-                                    header.setText(headerMessage);
-
-                                    String errorMessage = "Sorry, the course you specified could not be found.";
-                                    info.setText(errorMessage);
-                                }
+                                // reset adapter filter
+                                subject_adapter.getFilter().filter(editSubject.getText(), null);
+                            }
+                            catch (Exception e) {
+                                editSubject.setText(e.toString());
+                                e.printStackTrace();
                             }
                         }
                     }
-            );
+                }
+            });
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -107,19 +174,6 @@ public class MainActivity extends AppCompatActivity {
                     String.valueOf(infoObject.getInt("AvailableSeats")) + "\n";
             openClasses += append;
 
-                /*
-                // check that type (LEC/LAB/DIS) matches
-                if (!infoObject.getString("SectionType").isEmpty()) {
-                    // check if it is open
-                    if (infoObject.getInt("AvailableSeats") >= 0) {
-                        // add the course info to openClasses string
-                        String append = "Section " + infoObject.getString("SectionNumber") + ": " +
-                                String.valueOf(infoObject.getInt("AvailableSeats")) + "\n";
-                        openClasses += append;
-                        System.out.println(openClasses);
-                    }
-                }
-                */
         }
         // if no open classes are found...
         if (openClasses.length() == 0) {
@@ -129,14 +183,13 @@ public class MainActivity extends AppCompatActivity {
         return openClasses;
     }
 
-    @NonNull
     public static JSONArray getJSONArray(String url) throws IOException, JSONException {
 
         // Build and set timeout values for the request.
         System.out.println("HERE");
         URLConnection connection = (new URL(url)).openConnection();
         connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
+        connection.setReadTimeout(20000);
         connection.connect();
 
         // Read and store the result line by line then return the entire string.
@@ -149,5 +202,38 @@ public class MainActivity extends AppCompatActivity {
         in.close();
 
         return new JSONArray(html.toString());
+    }
+
+    public Map<String, String> readMapFromFile(String filename, String key_str, String value_str) {
+        try {
+            // read from json into JSONArray
+            InputStream is = getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
+            JSONArray arr = new JSONArray(json);
+
+            // convert JSONArray to Map
+            Map<String, String> school_map = new TreeMap<>();
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject obj = arr.getJSONObject(i);
+                // get key and value from JSON, put into map
+                String key = obj.getString(key_str);
+                String value = obj.getString(value_str);
+                school_map.put(key, value);
+            }
+            return school_map;
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        catch (JSONException j) {
+            j.printStackTrace();
+            return new HashMap<>();
+        }
     }
 }
