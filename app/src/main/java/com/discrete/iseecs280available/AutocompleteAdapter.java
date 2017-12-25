eadc8a06cfbfc47df99077fea368b29943841fad
Saad <saadmallick98@gmail.com>
2017-12-25 11:33:45 -0500

    Got AutocompleteAdapter working

--- app/src/main/java/com/discrete/iseecs280available/AutocompleteAdapter.java
+++ app/src/main/java/com/discrete/iseecs280available/AutocompleteAdapter.java
@@ -6,19 +6,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AutocompleteAdapter extends ArrayAdapter {
 
     private Context context;
-    private String[] data;
-    //private Typeface tf;
+    private ArrayList<String> data;
+    private ArrayList<String> filteredData;
 
-    public AutocompleteAdapter(Context context, String[] data) {
-        super(context, R.layout.drop_down_format, data);
+    public AutocompleteAdapter(Context context, String[] data_array) {
+        super(context, R.layout.drop_down_format, data_array);
         this.context = context;
-        this.data = data;
-        //this.tf = Typeface.createFromAsset(context.getAssets(), "fonts/Quicksand-Regular.otf");
+        this.data = new ArrayList<String>(Arrays.asList(data_array));
+        filteredData = this.data;
     }
 
     @Override
@@ -28,9 +32,39 @@ public class AutocompleteAdapter extends ArrayAdapter {
         View rowView = inflater.inflate(R.layout.drop_down_format, null);
         TextView courseName = (TextView) rowView.findViewById(R.id.text1);
 
-        courseName.setText(data[position]);
+        System.out.println(filteredData);
+        courseName.setText(filteredData.get(position));
 
         return rowView;
     }
 
+    /*
+    @Override
+    public Filter getFilter(){
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence)
+            {
+                FilterResults results = new FilterResults();
+                ArrayList<String> resultingData = new ArrayList();
+                for(String i: resultingData){
+                    if(charSequence.toString().substring(0, charSequence.length())
+                            .equals(i.substring(0, charSequence.length()))){
+                        resultingData.add(i);
+                    }
+                }
+                results.values = resultingData;
+                results.count = resultingData.size();
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults)
+            {
+                filteredData = (ArrayList<String>)filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+    */
 }
